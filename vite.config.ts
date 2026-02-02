39e53bcccd42edeb27587e380ea82bc18036f779
gin <rafly1955@gmail.com>
2026-02-02 14:47:47 +0700

    chore: configure frontend to connect to production backend

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,20 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: true, // Listen on all addresses
+    watch: {
+      // 3. tell vite to ignore watching `src-tauri`
+      ignored: ['**/src-tauri/**'],
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+  clearScreen: false,
 })
